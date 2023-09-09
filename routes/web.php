a4849a0b2f5d0b45a704d90e46236882707ce10f
Ghiyas Hafizh <agihafizh@gmail.com>
2023-09-09 11:26:20 +0700

    message

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,8 +27,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
-    Route::resource('contacts', ContactController::class);
+
+    Route:: resource('contacts',ContactController::class);
 });
 
 require __DIR__.'/auth.php';
